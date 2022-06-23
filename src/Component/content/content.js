f8ce18089fc9dcebfb6153c2ed8f3e027cc3e289
hanguyen2911 <ha.nguyen22@student.passerellesnumeriques.org>
2022-06-23 11:08:26 +0700

    uload file

--- src/Component/content/content.js
+++ src/Component/content/content.js
@@ -3,111 +3,128 @@ import axios from "axios";
 
 import React, { useEffect } from "react";
 import Card from "./conten/child";
+
+const priceFilters = [
+	{from: 1000000, to:2000000},
+	{from: 2000000, to:3000000},
+	{from: 3000000, to:4000000},
+	{from: 4000000, to:5000000},
+]
 function App() {
-	const [address, setaddress] = useState();
+  const [address, setaddress] = useState("");
+  const [priceFilter, setPriceFilter] = useState("");
 
-	const [namehotel, setnamehotel] = useState();
-	const [listProduct, setListProduct] = useState([]);
+  const [namehotel, setnamehotel] = useState("");
+  const [listProduct, setListProduct] = useState([]);
 
-	const getData = () => {
-		axios.get("http://localhost:3000/hotel").then((res) => {
-			setListProduct(res.data);
-		});
-	};
+  const getData = () => {
+    axios.get("http://localhost:3000/hotel").then((res) => {
+      setListProduct(res.data);
+    });
+  };
+	
+	const ProcessPrice =()=>{
 
-	//  const handlerChangePro = (e) =>{
-	//    const  key = e.target.name;
-	//    const value = e.target.value;
+	}
 
-	//     setProvince({ ...province, [key]: value });
-	//  };
-	//  console.log(province);
-	const handleSubmit = () => {
-		alert("loading");
-	};
-	useEffect(() => {
-		getData();
-	}, []);
-	return (
-		<>
-			<div className="container">
-				<form>
-					<div
-						className="form row aos-init aos-animate mt-5 mb-5"
-						data-aos="fade-up"
-					>
-						<div className="col-lg-3">
-							<div className="input-icon-wrap">
-								<input
-									type="text"
-									className="form-control flatpickr-input"
-									id="arrival"
-									placeholder="Province"
-									onChange={(e) => setaddress(e.target.value)}
-								/>
-							</div>
-						</div>
-						<div className="col-lg-3">
-							<div className="input-icon-wrap">
-								<input
-									type="text"
-									className="form-control flatpickr-input"
-									id="departure"
-									placeholder="Name hotel"
-									onChange={(e) => setnamehotel(e.target.value)}
-								/>
-							</div>
-						</div>
-						<div className="col-lg-3">
-							<div className="input-icon-wrap">
-								<span className="icon-person" />
-								<select id="number-of-person" className="form-control">
-									<option value>Price</option>
-									<option value>100.000 - 200.000VND</option>
-									<option value>400.000 - 500.000VND</option>
-									<option value>500.000 - 1000.000VND</option>
-									<option value>1000.000 - 2000.000VND</option>
-								</select>
-							</div>
-						</div>
-						<div className="col-lg-3">
-							<button
-								className="btn btn-primary btn-block w-100"
-								onClick={() => handleSubmit()}
-								style={{ width: "60px", height: "30px", marginLeft: "45%" }}
-							>
-								Find now
-							</button>
-						</div>
-					</div>
-				</form>
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+    setListProduct(listProduct
+										.filter(product => namehotel !== "" ? product.name.includes(namehotel) : true)
+										// .filter(product => priceFilter !== "" ? product.price >= 
+										// priceFilters[priceFilter].from && product.price <= priceFilters[priceFilter].to : true)
+										)
+  };
 
-				<div className="row">
-					{namehotel == ""
-						? listProduct.map((product) => (
-								<Card
-									image={product.image}
-									name={product.name}
-									price={product.price}
-									linkname={product.linkname}
-									address={product.address}
-								></Card>
-						  ))
-						: listProduct
-								.filter((product) => product.type == namehotel)
-								.map((product) => (
-									<Card
-										image={product.image}
-										name={product.name}
-										price={product.price}
-										linkname={product.linkname}
-										address={product.address}
-									></Card>
-								))}
-				</div>
-			</div>
-		</>
-	);
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <div
+            className="form row aos-init aos-animate mt-5 mb-5"
+            data-aos="fade-up"
+          >
+            <div className="col-lg-3">
+              <div className="input-icon-wrap">
+                <input
+                  type="text"
+                  className="form-control flatpickr-input"
+                  id="arrival"
+                  placeholder="Province"
+                  onChange={(e) => setaddress(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3">
+              <div className="input-icon-wrap">
+                <input
+                  type="text"
+                  className="form-control flatpickr-input"
+                  id="departure"
+                  placeholder="Name hotel"
+                  onChange={(e) => setnamehotel(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3">
+              <div className="input-icon-wrap">
+                <span className="icon-person" />
+                <select id="number-of-person" className="form-control" onChange={(e) => setPriceFilter(e.target.value)}>
+                  <option value>Price</option>
+                  <option value ='0'>100.000 - 200.000VND</option>
+                  <option value ='1'>400.000 - 500.000VND</option>
+                  <option value ='2'>500.000 - 1000.000VND</option>
+                  <option value ='3'>1000.000 - 2000.000VND</option>
+                </select>
+              </div>
+            </div>
+            <div className="col-lg-3">
+              <button
+                type="submit"
+                style={{ width: "60px", height: "30px", marginLeft: "45%" }}
+              >
+                Find now
+              </button>
+            </div>
+          </div>
+        </form>
+        <div>
+          <h1> {address}</h1>
+
+          {namehotel}
+        </div>
+        <div className="row">
+					{console.log(namehotel)}
+          {namehotel == "" ? (
+            listProduct.map((product) => (
+              <Card
+                image={product.image}
+                name={product.name}
+                price={product.price}
+                linkname={product.linkname}
+                address={product.address}
+              ></Card>
+            ))
+          ) : 
+					listProduct
+					.map((product) => (
+						<Card
+							image={product.image}
+							name={product.name}
+							price={product.price}
+							linkname={product.linkname}
+							address={product.address}
+						></Card>
+					))
+          }
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Lognout from './page/Lognout';
-import Register from './page/Register';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App/> */}
-    {/* <Lognout/>
-     */}
-     <Register/>
+    <App/>
+    {/* <Lognout/>*/}
+     {/* <Register/> */}
   </React.StrictMode>
 );
 
