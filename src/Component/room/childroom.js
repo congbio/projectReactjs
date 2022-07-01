8b0330f01a64ade97b7e6d25a6a2f536b22a9187
Phuong0210 <phuong.ho021021@gmail.com>
2022-07-01 18:30:53 +0700

    new update

--- src/Component/room/childroom.js
+++ src/Component/room/childroom.js
@@ -5,45 +5,35 @@ const Childroom = (props) => {
 
 	// const [id, setID] = useState(null);
 
-	const bookingRoom =(id)=>{
-		
+	const bookingRoom = (id) => {
+
 		console.log("thành công");
 		localStorage.setItem("idroom", id);
-		
+
 	}
 	return (
 		<>
+
 			<div className="col-lg-4" >
 				<div className="single-location mb-3">
-					<img src={props.image} alt="image hotel" />
-					<div className="location-contents">
-						<h3>
-							<a  >{props.name} </a>
-						</h3>
-						<div className="price">
-							Started from<span>{props.price}</span>{" "}
-						</div>
-						<div className="price">
-							<span>{props.description}</span>{" "}
-						</div>
-						<div className="price">
-							<span>{props.numberbed}</span>{" "}
-						</div>
-						<span>{props.area}</span> <br />
-						<span>{props.numberbed}</span>
-						<div className="price">
-							<button
-								type="button"
-								onClick={() => bookingRoom(props.id)}
-								className="btn btn-success"
-							>
+					<div className="card" style={{ width: '22rem' }}>
+						<img src={props.image} alt="image hotel" />
+						<div className="card-body">
+							<h5 className="card-title"><a  >{props.name} </a></h5>
+							<h6 className="card-title"><b>Giá:</b> {props.price} </h6>
+							<h6 className="card-title"><b>Mô tả:</b> <span>{props.numberbed}</span> <span>{props.area}</span></h6>
+							<p className="card-text"><span>{props.description}</span></p>
+							<a href="#" className="btn btn-warning"
+								onClick={() => bookingRoom(props.id)}>
 								<Link to={"/checkout"} className="nav-link">
 									Book
 								</Link>
-							
-							</button>
+
+							</a>
 						</div>
 					</div>
+
+
 				</div>
 			</div>
 		</>
